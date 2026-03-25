9e9a2e671783f7487173b84abd70593c3b733c9c
Krzysztof Zieliński <kzielinski50@gmail.com>
2026-03-25 19:16:47 +0100

    Add dismissible functionality to ExpensesList for expense removal

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -3,15 +3,31 @@ import 'package:expense_tracker/widgets/expenses_list/expense_item.dart';
 import 'package:flutter/material.dart';
 
 class ExpensesList extends StatelessWidget {
-  const ExpensesList({super.key, required this.expenses});
+  const ExpensesList({
+    super.key,
+    required this.expenses,
+    required this.onRemoveExpense,
+  });
 
   final List<Expense> expenses;
+  final void Function(Expense expense) onRemoveExpense;
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expenses.length,
-      itemBuilder: (ctx, index) => ExpenseItem(expenses[index]),
+      itemBuilder: (ctx, index) => Dismissible(
+        // Dismissible is a widget that allows the user to swipe to dismiss the widget, it's used to delete an expense from the list
+        key: ValueKey(
+          expenses[index],
+        ), // ValueKey is used to identify the widget that should be dismissed, it takes a unique value (in this case, the expense object itself) to identify the widget
+        onDismissed: (direction) {
+          onRemoveExpense(
+            expenses[index],
+          ); // call the onRemoveExpense function passed from the parent widget (Expenses) to remove the expense from the list
+        },
+        child: ExpenseItem(expenses[index]),
+      ),
     );
   }
 }
