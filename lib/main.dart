3de3640d5ab2bad4dd3f69aefe818d17bbf44745
Krzysztof Zieliński <kzielinski50@gmail.com>
2026-03-27 20:24:13 +0100

    Add dark theme configuration to enhance UI for dark mode users

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,27 @@ var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 96, 59, 181),
 );
 
+var kDarkColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+  brightness: Brightness.dark,
+);
+
 void main() {
   runApp(
     MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        cardTheme: const CardThemeData().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          ),
+        ),
+      ),
       theme: ThemeData().copyWith(
         colorScheme: kColorScheme,
         appBarTheme: const AppBarTheme().copyWith(
@@ -31,6 +49,8 @@ void main() {
           ),
         ),
       ),
+      themeMode: ThemeMode
+          .dark, // ThemeMode.system, // this will use the system theme (light or dark) based on the user's device settings
       home: const Expenses(),
     ),
   );
