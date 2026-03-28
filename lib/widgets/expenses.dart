9a625273ccf114c2f7aba9efaba1ad3b544cb517
Krzysztof Zieliński <kzielinski50@gmail.com>
2026-03-28 15:38:47 +0100

    Refactor layout in Expenses widget for responsive design based on screen width

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -71,6 +71,8 @@ class _Expenses extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+
     Widget mainContent = const Center(
       child: Text('No expenses found. Start adding some!'),
     );
@@ -92,12 +94,19 @@ class _Expenses extends State<Expenses> {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Chart(expenses: _registeredExpenses),
-          Expanded(child: mainContent),
-        ],
-      ),
+      body: width < 600
+          ? Column(
+              children: [
+                Chart(expenses: _registeredExpenses),
+                Expanded(child: mainContent),
+              ],
+            )
+          : Row(
+              children: [
+                Expanded(child: Chart(expenses: _registeredExpenses)),
+                Expanded(child: mainContent),
+              ],
+            ),
     );
   }
 }
